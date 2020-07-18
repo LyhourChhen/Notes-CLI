a900c07de592c28e17c7c8da1f1b9bacb54fcfa1
LyhourChhen <ukamilove66@gmail.com>
2020-07-18 13:08:08 +0700

    modified note function

--- src/note.ts
+++ src/note.ts
@@ -71,6 +71,18 @@ export const deleteNote = (id: any) => {
 
 export const modifiedNote = (args: NoteInterface) => {
   const notes = loadNote();
+  const index: number = notes.findIndex((note) => note.id === args.id);
+  console.log("render index", index);
+  if (index !== null || undefined) {
+    // @ts-ignore
+    notes[index].title = args.title;
+    // @ts-ignore
+    notes[index].body = args.body;
+    // @ts-ignore
+    saveNotes(notes);
+  } else {
+    console.log("We can't find your note base on your id you provided");
+  }
 };
 
 // Function Helper
